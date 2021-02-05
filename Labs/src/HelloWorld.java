7822cf3f5b6a63d5a8e4d02baa85479210b49121
synntacs <78395319+synntacs@users.noreply.github.com>
2021-02-04 21:42:37 -0700

    Update HelloWorld.java

--- Labs/src/HelloWorld.java
+++ Labs/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world from **change-to-your-name**");
+		System.out.println("Hello world from Anthony Duong");
 	}
 }
